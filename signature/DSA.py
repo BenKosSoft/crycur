7a389d498fd08be1c8b17a8018822bba952bce89
M.Mucahid Benlioglu <mbenlioglu@sabanciuniv.edu>
2017-12-13 01:54:36 +0300

    Digital signature init

---
+++ signature/DSA.py
@@ -0,0 +1,21 @@
+"""
+    Created by mbenlioglu & mertkosan on 12/13/2017
+"""
+from random import randint
+from pyprimes import primes
+
+
+def DL_Param_Generator(small_bound, large_bound):
+    pass
+
+
+def KeyGen(p, q, g):
+    pass
+
+
+def SignGen(m, p, q, g, alpha, beta):
+    pass
+
+
+def SignVer(m, r, s, p, q, g, beta):
+    pass
---
+++ signature/TxGen.py
@@ -0,0 +1,7 @@
+"""
+    Created by mbenlioglu & mertkosan on 12/13/2017
+"""
+
+
+def GenSingleTx(p, q, g, alpha, beta):
+    pass
