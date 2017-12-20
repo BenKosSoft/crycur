4751a3a2a347b23d09564e4548adaedc22e98733
M.Mucahid Benlioglu <mbenlioglu@sabanciuniv.edu>
2017-12-20 19:39:19 +0300

    Added new files for full chain generation

---
+++ chaining/PoW.py
@@ -0,0 +1,7 @@
+"""
+    Created by mbenlioglu on 12/20/2017
+"""
+
+
+def PoW (TxBlockFile, ChainFile, PoWLen, TxLen):
+    pass
---
+++ chaining/TxBlockGen.py
@@ -0,0 +1,7 @@
+"""
+    Created by mbenlioglu & mertkosan on 12/20/2017
+"""
+
+
+def GenTxBlock(p, q, g, count):
+    pass
